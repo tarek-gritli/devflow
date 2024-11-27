2283c9100788fdad6e4c4a94c15e952dcc17d827
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-11-27 17:51:05 +0100

    Setup tailwind themes & styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-black text-white">DevFlow</h1>
+      <h1 className="text-3xl font-black text-primary-500">DevFlow</h1>
     </>
   );
 }
