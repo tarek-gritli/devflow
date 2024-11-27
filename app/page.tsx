33cd45c98b382be97000b6943d206f3d7ffcca98
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-11-27 18:16:25 +0100

    Setup fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-black text-primary-500">DevFlow</h1>
+      <h1 className="h1-bold">DevFlow</h1>
+      <h1 className="h1-bold font-inter">DevFlow (Inter)</h1>
+      <h1 className="h1-bold font-space-grotesk">DevFlow (SpaceGrotesk)</h1>
     </>
   );
 }
