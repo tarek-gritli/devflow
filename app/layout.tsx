f92bd1d5b7415a2fbb567daf4270ed8c4bfb0b53
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-11-30 23:16:55 +0100

    Setup auth layouts, social auth form, next auth and oauth with google and github

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import React from "react";
 
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
+import { Toaster } from "@/components/ui/toaster";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -27,26 +29,31 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+const RootLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="h1-bold">DevFlow</h1>
-      <h1 className="h1-bold font-inter">DevFlow (Inter)</h1>
-      <h1 className="h1-bold font-space-grotesk">DevFlow (SpaceGrotesk)</h1>
-    </>
-  );
-}
