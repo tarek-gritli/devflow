e005e3f5045d8ac42cc33666d0b63423fc4571dd
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-12-01 21:17:33 +0100

    Implement auth form for sign in and sign up

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true })}
+    />
+  );
 };
 
 export default SignUp;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,9 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
   return (
     <>
       <h1 className="h1-bold">DevFlow</h1>
       <h1 className="h1-bold font-inter">DevFlow (Inter)</h1>
       <h1 className="h1-bold font-space-grotesk">DevFlow (SpaceGrotesk)</h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
