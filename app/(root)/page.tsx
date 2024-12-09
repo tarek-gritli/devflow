3ebbf09b62bb43e106fe8c1224ec47f83b51b75b
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-12-09 12:42:27 +0100

    Implement Local Search Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,71 @@
-const Home = async () => {
+import Link from "next/link";
+
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn react?",
+    description: "I want to learn react, can someone help me with that?",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: "2021-09-01T12:00:00.000Z",
+  },
+  {
+    _id: "2",
+    title: "How to learn Javascript?",
+    description: "I want to learn react, can someone help me with that?",
+    tags: [{ _id: "1", name: "javascript" }],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: "2021-09-01T12:00:00.000Z",
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
   return (
     <>
-      <h1 className="h1-bold">DevFlow</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          asChild
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      {/* HomeFilter */}
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
     </>
   );
 };
--- constants/routes.ts
+++ constants/routes.ts
@@ -5,6 +5,7 @@ const ROUTES = {
   PROFILE: (id: string) => `/profile/${id}`,
   QUESTION: (id: string) => `/question/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
+  ASK_QUESTION: "/ask-question",
 };
 
 export default ROUTES;
