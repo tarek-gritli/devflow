0223be4f576086393066fdaf4704de3d10115b66
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-12-14 22:31:23 +0100

    implement fetching user by email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+
+// POST /api/users/email
+export async function POST(request: Request) {
+  const { email } = await request.json();
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    await dbConnect();
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new NotFoundError("User");
+    }
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
