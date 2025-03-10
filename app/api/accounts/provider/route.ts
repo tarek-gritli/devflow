63263bcb77bd5f1f1766dd8fc17848e977dfcf81
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-03-10 01:51:59 +0100

    implement dynamic account by provider route

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+
+// POST /api/accounts/provider
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    await dbConnect();
+    const user = await Account.findOne({ providerAccountId });
+    if (!user) {
+      throw new NotFoundError("Account");
+    }
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
