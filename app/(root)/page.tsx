2472ac0bcb1f703f7d7d6df29d72383beb27e8f0
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-04-18 22:09:04 +0100

    add oauth authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 
+import { auth } from "@/auth";
 import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { api } from "@/lib/api";
-import handleError from "@/lib/handlers/error";
 
 const questions = [
   {
@@ -46,20 +45,12 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
-
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
-  console.log(users);
+  const session = await auth();
+  console.log(session);
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
--- constants/routes.ts
+++ constants/routes.ts
@@ -6,6 +6,7 @@ const ROUTES = {
   QUESTION: (id: string) => `/question/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
   ASK_QUESTION: "/ask-question",
+  SIGN_IN_WITH_OAUTH: "signin-with-oauth",
 };
 
 export default ROUTES;
