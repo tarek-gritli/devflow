e31d6816c212c06fff29a9a837a6986cdd2be035
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-09-06 17:12:00 +0100

    feat: implement answer creation and fetching functionality

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -3,11 +3,14 @@ import { redirect } from "next/navigation";
 import { unstable_after as after } from "next/server";
 import React from "react";
 
+import AllAnswers from "@/components/answers/AllAnswers";
 import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
+import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
+import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 
@@ -16,6 +19,17 @@ const QuestionDetails = async ({ params }: RouteParams) => {
   const { success, data: question } = await getQuestion({ questionId: id });
   after(async () => {
     await incrementViews({ questionId: id });
+  });
+
+  const {
+    success: areAnswersLoaded,
+    data: answersResult,
+    error: answersError,
+  } = await getAnswers({
+    questionId: id,
+    page: 1,
+    pageSize: 10,
+    filter: "latest",
   });
 
   if (!success || !question) return redirect("/404");
@@ -86,6 +100,19 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           />
         ))}
       </div>
+
+      <section className="my-5">
+        <AllAnswers
+          data={answersResult?.answers}
+          success={areAnswersLoaded}
+          error={answersError}
+          totalAnswers={answersResult?.totalAnswers || 0}
+        />
+      </section>
+
+      <section className="my-5">
+        <AnswerForm questionId={question._id} />
+      </section>
     </>
   );
 };
