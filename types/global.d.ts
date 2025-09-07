3bb4065ab1cb90f55fd9259a8558b06c5378e818
tarek-gritli <tarek.gritli@insat.ucar.tn>
2025-09-07 08:18:13 +0100

    feat: implement community page

--- types/global.d.ts
+++ types/global.d.ts
@@ -59,3 +59,15 @@ interface Answer {
   upvotes: number;
   downvotes: number;
 }
+
+interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
